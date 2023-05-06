1cdde18a380fc2fd696f32f744c87ce5c042a934
Viktor Taraba <55559682+viktor-taraba@users.noreply.github.com>
2023-05-07 02:16:57 +0300

    Add date_insert, time_insert
    Add date_insert, time_insert, change datatype dividends(tb_dividends) to float(25)

--- Create tables.sql
+++ Create tables.sql
@@ -4,33 +4,36 @@ use [DividendPolicyResearch]
 
 create table dbo.tb_sp500_tickers(
 	ticker				varchar(5),
-	date_insert			datetime,
+	date_insert			date,
+	time_insert			time,
 	constraint PK_ticker primary key (ticker),
 );
 
 create table dbo.tb_company_info(
 	city				varchar(50),
 	"state"				varchar(50),
-	zip					varchar(50),
+	zip				varchar(50),
 	website				varchar(100),
 	industry			varchar(200),
 	sector				varchar(200),
 	longBusinessSummary varchar(5000),
-    marketCap			bigint,
+    marketCap				bigint,
 	symbol				varchar(5),
-	underlyingSymbol	varchar(5),
+	underlyingSymbol		varchar(5),
 	shortName			varchar(300),
 	longName			varchar(500),
 	date_insert			datetime,
+	time_insert			time,
 	constraint FK_symbol foreign key (symbol)
 	references dbo.tb_sp500_tickers(ticker)
 );
 
 create table dbo.tb_dividends(
 	"date"				date,
-	dividends			numeric(7,6),
+	dividends			float(25),
 	ticker				varchar(5),
 	date_insert			datetime,
+	time_insert			time,
 	constraint FK_ticker foreign key (ticker)
 	references dbo.tb_sp500_tickers(ticker)
 );
