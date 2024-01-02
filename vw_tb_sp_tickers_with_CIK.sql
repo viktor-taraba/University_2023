a0afb740fa281411933f926eca3dacb93949b7ec
Viktor Taraba <55559682+viktor-taraba@users.noreply.github.com>
2024-01-03 00:30:24 +0200

    update vw_tb_sp_tickers_with_CIK definition (add field without_dividends)

--- vw_tb_sp_tickers_with_CIK.sql
+++ vw_tb_sp_tickers_with_CIK.sql
@@ -1,6 +1,9 @@
 create view [dbo].[vw_tb_sp_tickers_with_CIK] as
 select tt.*, 
-	   t.cik_str 
+	   t.cik_str,
+	   case when wod.ticker is not null then 1 else 0 end as without_dividends
 from dbo.tb_sp100_tickers tt
 join dbo.tb_CIK_ticker t
-	on tt.ticker = t.ticker;
+	on tt.ticker = t.ticker
+left join dbo.vw_stock_wo_dividends as wod
+	on wod.ticker = tt.Ticker;
