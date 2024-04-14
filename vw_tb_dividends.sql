2d687f2c8bf176f1a9e9e257031b4f1448ee73e1
Viktor Taraba <55559682+viktor-taraba@users.noreply.github.com>
2024-04-14 20:51:45 +0300

    Add files via upload

--- vw_tb_dividends.sql
+++ vw_tb_dividends.sql
@@ -6,4 +6,4 @@ select year([date]) as year_
       ,[ticker]
   from [DividendPolicyResearch].[dbo].[tb_dividends]
   where year([date]) >= 2013
-		and date_insert = '2024-01-02';
+		and date_insert = (select max(date_insert) from [DividendPolicyResearch].[dbo].[tb_dividends])
