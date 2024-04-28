7cbcc8c9736f729feb1ceefff4e8017cd23aedcd
Viktor Taraba <55559682+viktor-taraba@users.noreply.github.com>
2024-04-28 14:24:43 +0300

    Add files via upload

---
+++ Query for PBI.sql
@@ -0,0 +1,18 @@
+select  ticker, 
+		Year, 
+		case when ticker = 'TGT' and Year = 2022 then yf_Dividends
+			 when ticker = 'COF' and Year = 2021 then yf_Dividends
+			 when ticker = 'LIN' then yf_Dividends
+			 when ticker = 'MA' and Year = 2021 then dividend_nd 
+			 when ticker = 'SPG' and Year = 2014 then dividend_nd
+			 when ticker = 'DHR' and Year = 2015 then dividend_nd
+			 else fs_dividends
+		end as amount, 
+		'DPS' as type
+from [DividendPolicyResearch].[dbo].[tb_FS_Dividends_check]
+union all 
+select  ticker_name as ticker, 
+		Year, 
+		[Basic EPS] as amount, 
+		'EPS' as type
+from [DividendPolicyResearch].[dbo].[tb_FS_EPS_check]
